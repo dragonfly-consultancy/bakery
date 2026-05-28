29a20ad1eb2db0f923afaa430bc6e5e32e7db587
Krishan Bandara <80022206+Kishan-Bandara@users.noreply.github.com>
2026-05-29 00:00:55 +0530

    get  url file
    get  url file

--- admin/get_url.php
+++ admin/get_url.php
@@ -1,10 +1,31 @@
 <?php
-function redirect($url, $permanent = false) {
-	
-	if($permanent) {
+
+function getAdminBaseUrl()
+{
+	$documentRoot = isset($_SERVER['DOCUMENT_ROOT']) ? str_replace('\\', '/', rtrim($_SERVER['DOCUMENT_ROOT'], '/')) : '';
+	$projectRoot = str_replace('\\', '/', realpath(__DIR__ . '/..'));
+
+	if ($documentRoot && $projectRoot && stripos($projectRoot, $documentRoot) === 0) {
+		$relativePath = substr($projectRoot, strlen($documentRoot));
+		return 'http://' . $_SERVER['HTTP_HOST'] . rtrim(str_replace('\\', '/', $relativePath), '/');
+	}
+
+	return 'http://' . $_SERVER['HTTP_HOST'] . '/backary_krishan';
+}
+
+function redirect($url, $permanent = false)
+{
+	if ($permanent) {
 		header('HTTP/1.1 301 Moved Permanently');
 	}
-	header('Location:http://58.168.224.179:8050/bakery/admin/'.$url);
+
+	if (preg_match('#^https?://#i', $url)) {
+		header('Location: ' . $url);
+		exit();
+	}
+
+	$target = rtrim(getAdminBaseUrl(), '/') . '/admin/' . ltrim($url, '/');
+	header('Location: ' . $target);
 	exit();
 }
 
